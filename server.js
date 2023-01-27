075126b0779c92a02045eafdbb79836e1303a2a2
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-01-27 08:51:39 -0500

    restructured file path, unnecessary host folder

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = required('express')
 const app = express ()
+const PORT = 3000
 
 app.get('/', (req, res) => {
-    console.log('Here')
     res.send('Hi')
 })
 
-app.listen(3001)
+app.listen(PORT, () => {
+console.log(`Listening on port ${PORT}`)
+})
