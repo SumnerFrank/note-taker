d4834d94def92c93333f755c2dc67d21c4064135
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-01-28 14:14:17 -0500

    received error requiring middleware

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const express = required('express')
+const express = require('express')
 const app = express ()
+const fs = require('fs');
+const path = require('path');
 const PORT = 3000
-
 const apiRoute = require('./routes/api');
 const htmlRoute = require('./routes/html');
 
-app.get('/', (req, res) => {
-    res.send('Hi')
-})
+app.use(express.urlencoded({extended: true}));
+app.use('/api', apiRoute);
+app.use('/', htmlRoute);
 
 app.listen(PORT, () => {
 console.log(`Listening on port ${PORT}`)
