4b7cc05f318e7c586495485663f45fcc3bc36954
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-01-31 07:24:12 -0500

    added id to db

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const app = express ()
-const fs = require('fs');
-const path = require('path');
 const PORT = process.env.PORT || 3001;
 const apiRoute = require('./routes/api');
 const htmlRoute = require('./routes/html');
