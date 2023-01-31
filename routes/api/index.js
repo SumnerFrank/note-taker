432ea5c693a32d3d806db2cc809ec9512412385e
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-01-31 07:50:28 -0500

    updated index w/ const for note actions

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,24 @@
+const path = require('path');
+const notes = require('../../db.json')
+const uuid = require('../../helpers/uuid.js')
+const router = require('express').Router();
+const fs = require('fs');
+const createNote = require('../../lib/index.js');
+const deleteNote = require('../../lib/index.js');
+const showNotes = require('../../lib/index.js');
+const findID = require('../../lib/index.js');
+
+
 router.get('/', (req,res) => {
     res.json(notes);
 });
 
 router.post('/', (req,res) => {
-    res.json(notes);
+    req.body.id = notes.length.toString()
+    const note = createNote(req.body, notes)
+    res.json(note);
 });
 
+//need function to delete note 
 
 module.exports = router;
